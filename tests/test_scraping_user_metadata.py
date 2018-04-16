25345dcf88ba55a0818a68b68be2449abf272240
pocin <nemeth.robin@gmail.com>
2018-04-16 15:40:58 +0200

    scraping invalid (private/...) post url yields empty data
    TODO: How to distinguish it from too many requests/timeouts/whatever?

--- tests/test_scraping_user_metadata.py
+++ tests/test_scraping_user_metadata.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 from main import Scraper
 
 VALID = {
@@ -6,12 +7,25 @@ VALID = {
     "post_user_handle": os.environ['VALID_USER_HANDLE']
 }
 
-# INVALID = {
-#     "post_url": 'https://www.instagram.com/p/BhlUmAjnzSS/#1337_1757901802426021010_1967348397'
-# }
+INVALID = {
+    "post_url": os.environ['INVALID_POST_URL']
+}
 
 def test_scraping_valid_post_is_ok():
     scraper = Scraper()
     data = scraper.user_metadata_from_post(VALID['post_url'])
     assert data.keys() == Scraper.user_data_template.keys()
     assert data['user_handle'] == VALID['post_user_handle']
+
+
+def test_scraping_invalid_post_returns_empty_data(capsys):
+    url = INVALID['post_url']
+    scraper = Scraper()
+    data = scraper.user_metadata_from_post(url)
+    captured = capsys.readouterr()
+    assert "Mocking the data with `None` values" in captured.out
+    assert "Error getting data for url " + url in captured.out
+
+    assert data.keys() == Scraper.user_data_template.keys()
+    assert data['user_handle'] is None
+    assert data['url'] == url
